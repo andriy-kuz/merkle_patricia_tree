09e8809f00e663fde8f70975dd27bc098a32c38e
Adnriy Kuz <ext-andrii.kuz@here.com>
2018-03-13 16:35:02 +0200

    Get function sketche

--- src/tree.rs
+++ src/tree.rs
@@ -24,10 +24,43 @@ impl<T: Encodable + Decodable + Clone> MerkleTree<T> {
     pub fn update(&mut self, key: H256, value: T) {}
 
     pub fn delete(&mut self, key: &H256) {
-        self.db.delete_value(key);
     }
 
-    pub fn get(&self, key: &H256) -> Option<T> {
+    pub fn get(&mut self, key: &H256) -> Option<T> {
+        let key = Self::key_bytes_to_hex(key);
+        Self::get_helper(&self.db, &key[..], &mut self.root)
+    }
+
+    fn get_helper(db: &Database, key_path: &[u8], node: &mut Node<T>) -> Option<T> {
+        match node {
+            &mut Node::FullNode {ref mut nibles, ref flags} => {
+                if key_path.is_empty() {
+                    if let Some(ref mut node) = nibles[16] {
+                        return Self::get_helper(db, key_path, node)
+                    }
+                    return None
+                }
+                if let Some(ref mut node) = nibles[key_path[0] as usize] {
+                    return Self::get_helper(db, &key_path[1..], node)
+                }
+                return None
+            },
+            &mut Node::ShortNode {ref key, ref mut node, ref flags} => {
+                if key[..] == key_path[..key.len()] {
+                    return Self::get_helper(db, &key_path[key.len()..], node)
+                }
+                return None
+            },
+            &mut Node::HashNode {ref hash} => {
+                //TODO
+            },
+            &mut Node::ValueNode {ref value} => {
+                return Some(value.clone())
+            },
+            &mut Node::Null => {
+                return None
+            }
+        }
         None
     }
 
